e420ea68e6d3a5a00670f1223620a9fde1db318a
gxddem <gxddem@gmail.com>
2025-04-17 11:39:21 +0300

    feat: [#3] move db healthcheck to entyproint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
+#db healthcheck
+echo "Waiting for database to be ready"
+for i in {1..60}; do
+  if pg_isready -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -U ${POSTGRES_USER} > /dev/null 2>&1; then
+    echo "Database is ready"
+    break
+  else
+    echo "Waiting for database (${i}/60)"
+    sleep 1
+  fi
+done
+
+if [ $i -eq 60 ]; then
+  echo "Database did not become ready in time. Exiting"
+  exit 1
+fi
+
+
 #run migrations
-echo "Running migrations..."
+echo "Running migrations"
 migrate -path /app/migrations -database "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable" up
 
 if [ $? -ne 0 ]; then
-    echo "Migrations failed!"
+    echo "Migrations faile!"
     exit 1
 fi
-echo "Migrations applied successfully!"
+echo "Migrations applied successfully"
 
-echo "Starting application..."
+echo "Starting application"
 exec "$@"
